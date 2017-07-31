0204400e6d0c678566e099510ca810f6a08af05f
JohnSimerlink <johnsimerlink@gmail.com>
2017-07-30 17:12:47 -0700

    edit button shows on mouseover content node

--- app/components/tree/treecomponent.js
+++ app/components/tree/treecomponent.js
@@ -1,6 +1,7 @@
 import {Trees} from '../../objects/trees'
 import {Facts} from '../../objects/facts'
 import {Fact} from '../../objects/fact'
+import ContentItem from '../../objects/contentItem'
 import timers from './timers'
 import PubSub from 'pubsub-js'
 export default {
@@ -12,13 +13,22 @@ export default {
         this.editing = false
         this.tree = {} // init to empty object until promises resolve, so vue does not complain
         this.fact = {}
-        Trees.get(this.id).then( (tree) => {
+        this.content = {}
+        Trees.get(this.id).then(tree => {
             self.tree = tree
-            Facts.get(tree.factId).then((fact) =>{
-                self.fact = fact
-                this.startTimer()
+            ContentItem.get(tree.contentId).then(content => {
+                self.content = content
+                self.startTimer()
             })
+
         })
+        // Trees.get(this.id).then( (tree) => {
+        //     self.tree = tree
+        //     Facts.get(tree.factId).then((fact) =>{
+        //         self.fact = fact
+        //         this.startTimer()
+        //     })
+        // })
         PubSub.subscribe('canvas.clicked', () => {
             self.saveTimer()
         })
@@ -27,13 +37,11 @@ export default {
         return {
              tree: this.tree
             , fact: this.fact
-            , typeIsHeader: false
-            , typeIsFact: true
         }
     },
     computed : {
-        typeIsHeader() {
-            return this.tree.contentType == 'header'
+        typeIsHeading() {
+            return this.tree.contentType == 'heading'
         },
         typeIsFact() {
             return this.tree.contentType == 'fact'
@@ -42,19 +50,19 @@ export default {
     methods: {
         startTimer() {
             var self = this
-            if (!timers[this.fact.id]){ // to prevent two timers from being set on the same fact simultaneously (two back to back mousevers in sigmajs will do that, causing two seconds to increment every one second
+            if (!timers[this.content.id]){ // to prevent two timers from being set on the same fact simultaneously (two back to back mousevers in sigmajs will do that, causing two seconds to increment every one second
                 setInterval(function(){
-                    self.fact.timeElapsedForCurrentUser = self.fact.timeElapsedForCurrentUser || 0
-                    self.fact.timeElapsedForCurrentUser++ // = fact.timeElapsedForCurrentUser || 0
+                    self.content.timeElapsedForCurrentUser = self.content.timeElapsedForCurrentUser || 0
+                    self.content.timeElapsedForCurrentUser++ // = fact.timeElapsedForCurrentUser || 0
                     // console.log('increment', self.x, self.y)
                 }, 1000)
 
-                timers[this.fact.id] = true
+                timers[this.content.id] = true
             }
         },
         saveTimer() {
-            this.fact.setTimerForUser && this.fact.setTimerForUser(this.fact.timeElapsedForCurrentUser)
-            timers[this.fact.id] = false
+            this.content.setTimerForUser && this.content.setTimerForUser(this.content.timeElapsedForCurrentUser)
+            timers[this.content.id] = false
         },
         toggleEditing() {
             this.editing = !this.editing
